ca0c372e7ae1d23032f56a55c7d9b91183c8a66d
goshofleksa82 <156816793+goshofleksa82@users.noreply.github.com>
2024-10-06 15:20:50 +0300

    Start up

---
+++ SRC/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Just works');
+})
+
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'))
+
