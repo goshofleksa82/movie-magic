77a97d811c1ef5e0ad45da1cf5c4453befc28e47
goshofleksa82 <156816793+goshofleksa82@users.noreply.github.com>
2024-10-11 17:32:00 +0300

    Add movie rating

--- SRC/controllers/movieController.js
+++ SRC/controllers/movieController.js
@@ -18,7 +18,17 @@ router.post('/create', async (req, res) => {
 router.get('/:movieId/details', async (req, res) => {
  const movieId = req.params.movieId;
   const movie  = await movieService.getOne(movieId)
+
+movie.ratingView = getRatingViewData(movie.rating);
  
   res.render('movies/details', { movie })
 })
+
+function getRatingViewData(rating){
+  if(!Number.isInteger(rating)){
+    return 'n\\a';
+  }
+  
+  return '&#x2605;'.repeat(rating);
+}
 export default router;
