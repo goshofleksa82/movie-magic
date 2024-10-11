7dfcc4bc91315a589693f3c49cafb205f90b8b5a
goshofleksa82 <156816793+goshofleksa82@users.noreply.github.com>
2024-10-11 18:34:41 +0300

    Add search page

--- SRC/services/movieService.js
+++ SRC/services/movieService.js
@@ -1,26 +1,40 @@
 import uniqid from 'uniqid';
 
- import movieData from "../data/movieData.js";
+import movieData from "../data/movieData.js";
 
-const getAll = () => movieData.getAll();
+const getAll = async (filter = {}) => {
+    let movies = await movieData.getAll();
+
+    if (filter.search) {
+        movies = movies.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
+    }
+    if (filter.genre) {
+        movies = movies.filter(movie => movie.genre.toLowerCase() === filter.genre.toLowerCase());
+    }
+    if (filter.year) {
+        movies = movies.filter(movie => movie.year === filter.year);
+    }
+
+    return movies;
+}
 
 const create = (movie) => {
-    
+
     movie.id = uniqid();
     movie.rating = Number(movie.rating);
 
-   return movieData.create(movie);
+    return movieData.create(movie);
 };
 
 const getOne = async (movieId) => {
-const movies = await movieData.getAll();
+    const movies = await movieData.getAll();
 
-const resultMovie = movies.find(movie => movie.id == movieId);
- 
-return resultMovie;
+    const resultMovie = movies.find(movie => movie.id == movieId);
+
+    return resultMovie;
 }
 
-export default{
+export default {
     getAll,
     create,
     getOne
