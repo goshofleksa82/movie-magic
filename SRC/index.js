fec098a9ad1cf4e46f95071234cc081cd7f93bb1
goshofleksa82 <156816793+goshofleksa82@users.noreply.github.com>
2024-10-16 16:07:33 +0300

    Add DB config

--- SRC/index.js
+++ SRC/index.js
@@ -3,13 +3,14 @@ import express from 'express';
 import routes from './routes.js';
 import handlebarsInit from './config/handlebarsinit.js';
 import expressInit from './config/expressInit.js';
+import mongooseInit from './config/mongooseInit.js';
 
 const app = express();
 
+mongooseInit();
 handlebarsInit(app);
 expressInit(app);
 
 app.use(routes);
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'))
-
