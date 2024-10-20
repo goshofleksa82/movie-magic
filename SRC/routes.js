5d2eb84f95ba59b9629d223aedc3a8a5efa4264a
goshofleksa82 <156816793+goshofleksa82@users.noreply.github.com>
2024-10-20 15:39:51 +0300

    Add register page

--- SRC/routes.js
+++ SRC/routes.js
@@ -3,14 +3,16 @@ import { Router } from 'express';
 import homeController from './controllers/homeController.js';
 import movieController from './controllers/movieController.js';
 import castController from './controllers/castController.js';
+import authController from './controllers/authController.js';
 
 const router = Router();
 
 router.use(homeController);
 router.use('/movies', movieController);
 router.use('/casts', castController);
+router.use('/auth', authController);
 router.all('*', (req, res) => {
     res.render('404')
-})
+});
 
 export default router;
