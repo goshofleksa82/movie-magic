bb724fea98fcac66d8294cc9b2ddc7ea38441fdf
goshofleksa82 <156816793+goshofleksa82@users.noreply.github.com>
2024-10-22 16:53:59 +0300

    Generate jwt token on successful login

--- SRC/services/authService.js
+++ SRC/services/authService.js
@@ -1,5 +1,9 @@
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken';
+
 import User from "../models/User.js";
+
+const SECRET = '18ba07130b39f2fe9ca139e4e42f9a07d718aa4685f399b705253c2ed4b8f3c0';
 
 const register = (email, password) => {
     return User.create({ email, password });
@@ -22,6 +26,13 @@ const login = async (email, password) => {
    }
 
    //TODO: generate jwt token
+   const payload = { 
+    _id: user._id,
+    email, 
+};
+
+const token = jwt.sign(payload, SECRET, { expiresIn: '2h' });
+
 
    //TODO: Return jwt token
 }
