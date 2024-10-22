231b638bf16d579d919c203a798c3c1e994dc283
goshofleksa82 <156816793+goshofleksa82@users.noreply.github.com>
2024-10-22 16:43:35 +0300

    validate login password

--- SRC/controllers/authController.js
+++ SRC/controllers/authController.js
@@ -16,10 +16,20 @@ router.post('/register', async (req, res) => {
 
     res.redirect('/auth/login');
     
-})
+});
 
 router.get('/login', (req, res)=>{
     res.render('auth/login');
-})
+});
+
+router.post('/login', async (req, res)=>{
+   const {email, password} = req.body;
+
+   await authService.login(email, password);
+
+   //TODO: Add token to cookie
+
+   res.redirect('/');
+});
 
 export default router; 
--- SRC/services/authService.js
+++ SRC/services/authService.js
@@ -1,10 +1,32 @@
+import bcrypt from 'bcrypt'
 import User from "../models/User.js";
 
 const register = (email, password) => {
-return User.create({ email, password });
+    return User.create({ email, password });
 
+}
+
+const login = async (email, password) => {
+   //TODO: check if user exist
+   const user = await User.findOne({ email });
+
+   if(!user){
+    throw new Error('User does not exist!');
+   }
+
+   //TODO: validate password
+   const isValid = await bcrypt.compare(password, user.password);
+
+   if(!isValid){
+    throw new Error('PAssword does not match!');
+   }
+
+   //TODO: generate jwt token
+
+   //TODO: Return jwt token
 }
 
 export default {
     register,
+    login,
 }
